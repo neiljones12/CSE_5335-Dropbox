9323b74782971420eda62788afb3f4778ca4b567
Neil Jones <neil.j.neil@gmail.com>
2017-12-04 01:37:39 -0800

    initial setup

--- album.php
+++ album.php
@@ -1,12 +1,19 @@
+<!-- Submitted By: Neil Jones ( 1001371689 ) -->
 <?php
-
 // display all errors on the browser
 error_reporting(E_ALL);
 ini_set('display_errors','On');
-
-
-
-
-
-
 ?>
+
+<html> 
+    <head>
+        <title>Project 8 - Neil Jones</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossorigin="anonymous">
+    </head>
+
+    <body style="padding-top:30px">
+         
+    </body>
+
+</html>
